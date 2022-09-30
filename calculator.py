1ee316dd6ba60dc5a5fe238426d27cb3d4f6b27d
sosso2022 <mushiranzigo2017@gmail.com>
2022-09-30 23:01:59 +0200

    commit some changes

--- calculator.py
+++ calculator.py
@@ -1,18 +1,5 @@
 # calculator/calculations.py
 from typing import Union
-def add(a, b):
-    return float(a + b)
-
-def subtract(a, b):
-    return float(a - b)
-
-def multiply(a, b):
-    return float(a * b)
-
-def divide(a, b):
-    if b == 0:
-        raise ZeroDivisionError("division by zero")
-    return float(a / b)
 
 def add(a: Union[float, int], b: Union[float, int]) -> float:
     """Compute and return the sum of two numbers.
